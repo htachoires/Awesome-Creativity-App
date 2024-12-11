083d2471341d738bd75d955671f7fa8ef10e5521
Hugo Tachoires <hugo.tachoires@cdbdx.biz>
2024-12-11 23:34:07 +0100

    feat: add modal day 12

--- apps/awesome-creativity-app/app/ui/santa-modal/santa-modal-day-12/santa-modal-day-12.tsx
+++ apps/awesome-creativity-app/app/ui/santa-modal/santa-modal-day-12/santa-modal-day-12.tsx
@@ -5,19 +5,31 @@ export default function ModalDay12() {
     <div className={styles.textContainer}>
       <div className={styles.titleContainer}>
         <span className={styles.title}>
-          Pendant leur fuite, un lutin pirate a laissé tomber un autre cadeau
+          Le restaurant Youyou vous offre l&apos;entrée de votre choix pour un
+          moment culinaire à partager avec le chef ❤️
         </span>
       </div>
-      <div className={styles.planImgContainer}>
+      <div className={styles.imgContainer}>
         <img
-          className={styles.planImg}
-          src="/img/modal/9/plan-shadow.png"
-          alt="gift"
+          className={styles.massageImg}
+          src="/img/modal/12/bon-pour-une-entree.jpg"
+          alt="massage"
         />
       </div>
-      <div className={styles.hintContainer}>
-        <span className={styles.description}>
-          💎 Arriveras-tu à trouver le trouver ? 💎
+      <div className={styles.downloadContainer}>
+        <a
+          href="/img/modal/12/bon-pour-une-entree.jpg"
+          className={styles.link}
+          download
+        >
+          J&apos;enregistre mon bon 💾
+        </a>
+      </div>
+      <div className={styles.descriptionContainer}>
+        <span>
+          Attention ! Pour être activé, ce bon devra être présenté à ton Youyou
+          et est valide qu&apos;une seule fois 😉 Offre cumulable avec de
+          potentiel futur bon 🤫
         </span>
       </div>
     </div>
