65bd22ae6a1c417e10b5e96a058ff2a59dd6df47
Hugo Tachoires <hugo.tachoires@cdbdx.biz>
2024-12-20 19:45:59 +0100

    wip: add modal day 25

--- apps/awesome-creativity-app/app/ui/santa-modal/santa-modal-day-25/santa-modal-day-25.tsx
+++ apps/awesome-creativity-app/app/ui/santa-modal/santa-modal-day-25/santa-modal-day-25.tsx
@@ -1,24 +1,43 @@
 import styles from './santa-modal-day-25.module.scss';
+import { motion, Variants } from 'framer-motion';
 
-export default function ModalDay25() {
+export default function ModalDay24() {
+  const variant: Variants = {
+    move: {
+      y: [3, 0, 3],
+      transition: {
+        duration: 1, // Total time for one cycle (up and down)
+        repeat: Infinity, // Keep repeating
+        ease: 'easeInOut', // Smooth transitions
+      },
+    },
+  };
+
   return (
     <div className={styles.textContainer}>
       <div className={styles.titleContainer}>
         <span className={styles.title}>
-          Pendant leur fuite, un lutin pirate a laissé tomber un autre cadeau
+          Youyou a le droit à des cadeaux tous les jours, quel bonheur 🎁 Et
+          pourquoi pas mon chat aussi ? Ça tombe bien parce qu'on se voit ce
+          soiiir 🥰
         </span>
       </div>
-      <div className={styles.planImgContainer}>
+      <motion.div
+        animate={'move'}
+        variants={variant}
+        className={styles.imgContainer}>
         <img
-          className={styles.planImg}
-          src="/img/modal/9/plan-shadow.png"
-          alt="gift"
+          className={styles.massageImg}
+          src="/img/modal/20/gift.png"
+          alt="cinema"
         />
+      </motion.div>
+      <div className={styles.descriptionContainer}>
+        <span>Seul bémol, un peu de patience te sera requis... 🫣</span>
       </div>
-      <div className={styles.hintContainer}>
-        <span className={styles.description}>
-          💎 Arriveras-tu à trouver le trouver ? 💎
-        </span>
+      <div className={styles.imgBottomContainer}>
+        <img className={styles.lightLeft} src="/img/lights-left.png" alt="left" />
+        <img className={styles.lightRight} src="/img/lights-right.png" alt="left" />
       </div>
     </div>
   );
