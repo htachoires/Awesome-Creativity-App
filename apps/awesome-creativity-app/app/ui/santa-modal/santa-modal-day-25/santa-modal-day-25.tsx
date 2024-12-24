4aff9cc0636bf4944984065386df151e7d93956f
Hugo Tachoires <hugo.tachoires@cdbdx.biz>
2024-12-24 15:31:10 +0100

    wip: implement day 25

--- apps/awesome-creativity-app/app/ui/santa-modal/santa-modal-day-25/santa-modal-day-25.tsx
+++ apps/awesome-creativity-app/app/ui/santa-modal/santa-modal-day-25/santa-modal-day-25.tsx
@@ -1,9 +1,28 @@
 import styles from './santa-modal-day-25.module.scss';
 
-export default function ModalDay25() {
+export default function ModalDay24() {
   return (
-    <div className={styles.textContainer}>
-        <p>TODO</p>
+    <div className={styles.container}>
+      <p className={styles.firstParagraph}>TODO</p>
+      <div className={styles.gridImgContainer}>
+        <img
+          className={styles.gridImg}
+          src="/img/modal/25/left.jpg"
+          alt="left gift"
+        />
+        <img
+          className={styles.gridImg}
+          src="/img/modal/25/right.jpg"
+          alt="right gift"
+        />
+        <img
+          className={styles.mainImg}
+          src="/img/modal/25/gift.jpg"
+          alt="gift name"
+        />
+      </div>
+      <p className={styles.secondParagraph}>TODO</p>
+      <p className={styles.merryChristmas}>Joyeux Noël mon Chat ❤️</p>
     </div>
   );
 }
